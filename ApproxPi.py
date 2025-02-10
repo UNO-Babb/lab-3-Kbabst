2621817220e6a11fdb672b333e8b1a2d556d9910
Kbabst <kbabst@unomaha.edu>
2025-02-10 02:50:54 +0000

    Lab with ApproxPi

--- ApproxPi.py
+++ ApproxPi.py
@@ -10,12 +10,21 @@ import time
 
 def main():
   realPi = math.pi
-
+  prec = input("Input precision: ")
+  prec = int(prec)
   #ask user for decimal percision (up to 10)
 
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
+  approxPi = 4/1
+  sign = -1
+  denom = 3
+  while round(approxPi, prec) != round(realPi, prec):
+    print(approxPi)
+    approxPi = approxPi + (sign * 4 / denom)
+    sign = sign * -1
+    denom = denom + 2
 
   end = time.time()
 
